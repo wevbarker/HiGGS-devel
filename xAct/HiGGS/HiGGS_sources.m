e8c460c5bb3e96e8bdbd2142989e8097e66d78ec
Will <wb263@cam.ac.uk>
2022-03-17 23:55:27 +0000

    tinkering

--- xAct/HiGGS/HiGGS_sources.m
+++ xAct/HiGGS/HiGGS_sources.m
@@ -3640,24 +3640,24 @@ DefIfConstraintToTheoryNesterForm[$ToShellFreedoms_,$ToTheory_,$Theory_]:=Module
 (*a message*)
 xAct`xTensor`Private`MakeDefInfo[DefTheory,$Theory,{"$IfConstraintToTheoryNesterForm for the theory",""}];
 Phis=DeleteCases[Evaluate[{(1-ShellOrigB0p)PhiB0p[],(1-ShellOrigB1p)PhiB1p[-i,-j],(1-ShellOrigB1m)PhiB1m[-i],(1-ShellOrigB2p)PhiB2p[-i,-j],(1-ShellOrigA0p)PhiA0p[],(1-ShellOrigA0m)PhiA0m[],(1-ShellOrigA1p)PhiA1p[-i,-j],(1-ShellOrigA1m)PhiA1m[-i],(1-ShellOrigA2p)PhiA2p[-i,-j],(1-ShellOrigA2m)PhiA2m[-i,-j,-k]}/.$ToShellFreedoms],0,Infinity];
-$IfConstraints~AppendTo~Phis;
+$IfConstraints=$IfConstraints~Join~Phis;
 Phis=({#,ImposeTheory[#,$ToTheory]})&/@Phis;
 Print["** DefTheory: Found the following primary if-constraints:"];
 (Print[#[[1]]," \[Congruent] ",#[[2]]," \[TildeTilde] 0"])&/@Phis;
 Phis=DeleteCases[Evaluate[{(ShellOrigB0p)PhiB0p[],(ShellOrigB1p)PhiB1p[-i,-j],(ShellOrigB1m)PhiB1m[-i],(ShellOrigB2p)PhiB2p[-i,-j],(ShellOrigA0p)PhiA0p[],(ShellOrigA0m)PhiA0m[],(ShellOrigA1p)PhiA1p[-i,-j],(ShellOrigA1m)PhiA1m[-i],(ShellOrigA2p)PhiA2p[-i,-j],(ShellOrigA2m)PhiA2m[-i,-j,-k]}/.$ToShellFreedoms],0,Infinity];
 Phis=({#,ImposeTheory[#,$ToTheory]})&/@Phis;
 ChiPerps=DeleteCases[Evaluate[{(1-ShellPerpB0p)ChiPerpB0p[],(1-ShellPerpB1p)ChiPerpB1p[-i,-j],(1-ShellPerpB1m)ChiPerpB1m[-i],(1-ShellPerpB2p)ChiPerpB2p[-i,-j],(1-ShellPerpA0p)ChiPerpA0p[],(1-ShellPerpA0m)ChiPerpA0m[],(1-ShellPerpA1p)ChiPerpA1p[-i,-j],(1-ShellPerpA1m)ChiPerpA1m[-i],(1-ShellPerpA2p)ChiPerpA2p[-i,-j],(1-ShellPerpA2m)ChiPerpA2m[-i,-j,-k]}/.$ToShellFreedoms],0,Infinity];
-$IfConstraints~AppendTo~ChiPerps;
+$IfConstraints=$IfConstraints~Join~ChiPerps;
 ChiPerps=({#,ImposeTheory[#,$ToTheory]})&/@ChiPerps;
 Print["** DefTheory: Found the following secondary perpendicular if-constraints:"];
 (Print[#[[1]]," \[Congruent] ",#[[2]]," \[TildeTilde] 0"])&/@ChiPerps;
 ChiParas=DeleteCases[Evaluate[{(1-ShellParaB0m)ChiParaB0m[],(1-ShellParaB1p)ChiParaB1p[-i,-j],(1-ShellParaB1m)ChiParaB1m[-i],(1-ShellParaB2m)ChiParaB2m[-i,-j,-k],(1-ShellParaA0p)ChiParaA0p[],(1-ShellParaA0m)ChiParaA0m[],(1-ShellParaA1p)ChiParaA1p[-i,-j],(1-ShellParaA1m)ChiParaA1m[-i],(1-ShellParaA2p)ChiParaA2p[-i,-j],(1-ShellParaA2m)ChiParaA2m[-i,-j,-k]}/.$ToShellFreedoms],0,Infinity];
-$IfConstraints~AppendTo~ChiParas;
+$IfConstraints=$IfConstraints~Join~ChiParas;
 ChiParas=({#,ImposeTheory[#,$ToTheory]})&/@ChiParas;
 Print["** DefTheory: Found the following secondary parallel if-constraints:"];
 (Print[#[[1]]," \[Congruent] ",#[[2]]," \[TildeTilde] 0"])&/@ChiParas;
 ChiSings=DeleteCases[Evaluate[{(1-ShellSingB1p)ChiSingB1p[-i,-j],(1-ShellSingB1m)ChiSingB1m[-i],(1-ShellSingA0p)ChiSingA0p[],(1-ShellSingA0m)ChiSingA0m[],(1-ShellSingA1p)ChiSingA1p[-i,-j],(1-ShellSingA1m)ChiSingA1m[-i],(1-ShellSingA2p)ChiSingA2p[-i,-j],(1-ShellSingA2m)ChiSingA2m[-i,-j,-k]}/.$ToShellFreedoms],0,Infinity];
-$IfConstraints~AppendTo~ChiSings;
+$IfConstraints=$IfConstraints~Join~ChiSings;
 ChiSings=({#,ImposeTheory[#,$ToTheory]})&/@ChiSings;
 Print["** DefTheory: Found the following secondary singular if-constraints:"];
 (Print[#[[1]]," \[Congruent] ",#[[2]]," \[TildeTilde] 0"])&/@ChiSings;
