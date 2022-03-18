3d43d69788964c1e636a4bffb27d5143f5633792
Will <wb263@cam.ac.uk>
2022-03-18 13:37:10 +0000

    attempt at parallel load HiGGS

--- jobs.m
+++ jobs.m
@@ -20,27 +20,28 @@
 
 
 (* ::Input::Initialization:: *)
-(*Load a general form of xAct so that the contexts distributed are not just Global`*)
-Needs["xAct`xTras`"];
+(*Load HiGGS*)
+Needs["xAct`HiGGS`"];
+(*A list of jobs defined according to a theory and a name of that theory*)
+TheoryList={{"simple_spin_1+",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp6==0,Bet1==0,Bet2==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},{"simple_spin_1-",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp6==0,Bet1==0,Bet2==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},{"simple_spin_2-",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp6==0,Bet1==0,Bet2==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},{"simple_spin_0-2-",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp6==0,Bet1==0,Bet2==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}}};
 (*Batch job*)
 BatchJob[x_]:=Module[{},
 (*Start timing*)
 StartTime=AbsoluteTime[];
 (*Build the HiGGS environment*)
-xAct`HiGGS`BuildHiGGS[];
-(*
+BuildHiGGS[];
 (*Define the theory*)
-xAct`HiGGS`DefTheory[{Alp1\[Equal]0,Alp2\[Equal]0,Alp3\[Equal]0,Alp4\[Equal]0,Alp6\[Equal]0,Bet1==0,Bet2\[Equal]0,cAlp1==0,cAlp2\[Equal]0,cAlp3\[Equal]0,cAlp4\[Equal]0,cAlp5\[Equal]0,cAlp6\[Equal]0,cBet1\[Equal]0,cBet2\[Equal]0,cBet3\[Equal]0}];
+DefTheory[x[[2]]];
 (*List of constraints with fresh indices for PBs*)
-IndIfConstraints=(#~ChangeFreeIndices~({-l,-m,-n}~Take~Length@FindFreeIndices@#))&/@xAct`HiGGS`$IfConstraints;
+IndIfConstraints=(#~ChangeFreeIndices~({-l,-m,-n}~Take~Length@FindFreeIndices@#))&/@$IfConstraints;
 (*Evaluate lots of Poisson brackets*)
-xAct`HiGGS`PrimaryPoissonMatrix=Table[{xAct`HiGGS`$IfConstraints[[ii]],IndIfConstraints[[jj]],xAct`HiGGS`PoissonBracket[xAct`HiGGS`$IfConstraints[[ii]],IndIfConstraints[[jj]]]},{ii,Length@xAct`HiGGS`$IfConstraints},{jj,ii,Length@xAct`HiGGS`$IfConstraints}];
-*)
-TensorsList=$Tensors;
+PrimaryPoissonMatrix=Table[{$IfConstraints[[ii]],IndIfConstraints[[jj]],PoissonBracket[$IfConstraints[[ii]],IndIfConstraints[[jj]]]},{ii,Length@$IfConstraints},{jj,ii,Length@$IfConstraints}];
+(*Finish timing*)
 EndTime=AbsoluteTime[];
 TotTime=EndTime-StartTime;
 (*save to mx cache*)
-DumpSave[FileNameJoin@{Directory[],"bin","results-"<>ToString@x<>".mx"},{TotTime,TensorsList}];
+DumpSave[FileNameJoin@{Directory[],"bin","results-"<>ToString@x[[1]]<>".mx"},{TotTime,PrimaryPoissonMatrix}];
+(*It is very important to kill each kernel after the evaluation, otherwise it blocks the queue!*)
 Quit[];
 ];
 (*Make sure the BatchJob function is distributed over all parallel kernels*)
@@ -52,8 +53,7 @@ ParallelNeeds["xAct`HiGGS`"];
 (*Off we go...*)
 LaunchKernels[];
 (*Submit to the queue*)
-JobLabels=Table[ii,{ii,20}];
-Jobs=(ParallelSubmit@BatchJob@#)&/@JobLabels;
+Jobs=(ParallelSubmit@BatchJob@#)&/@TheoryList;
 (*{ParallelSubmit@BatchJob["firstjob"],ParallelSubmit@BatchJob["secondjob"],ParallelSubmit@BatchJob["thirdjob"]};*)
 (*Do the work*)
 WaitAll[Jobs];
