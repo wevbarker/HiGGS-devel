8eb540a8b2171c595085a6b26bd29a5b7a931b98
Will <wb263@cam.ac.uk>
2022-04-15 08:14:13 +0100

    spin 0 jobs

--- jobs.m
+++ jobs.m
@@ -40,7 +40,7 @@ DefTheory["Import"->"MyTheory"];
 
 JobsBatch={{"spin_0p",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp5==0,2Bet1+Bet2==0,Bet1+2Bet3==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},
 {"spin_0m",{Alp1==0,Alp2==0,Alp4==0,Alp5==0,Alp6==0,2Bet1+Bet2==0,Bet1+2Bet3==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}}};
-JobsBatch~StudyTheory~("Import"->True);
+JobsBatch~StudyTheory~("Import"->False);
 Quit[];
 
 
