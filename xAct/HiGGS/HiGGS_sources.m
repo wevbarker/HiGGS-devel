82ddd9b712f83f67810b2b45aa0d44156b013dc4
William Barker <wb263@mrao.cam.ac.uk>
2022-05-01 20:15:01 +0100

    tinker

--- xAct/HiGGS/HiGGS_sources.m
+++ xAct/HiGGS/HiGGS_sources.m
@@ -5333,8 +5333,8 @@ StudyTheory[InputBatch___:Null,OptionsPattern[]]:=Catch@Module[{DefinedTheories,
 (*We now want to change this module into something which studies batches of theories*)
 (*As long as the 2^- sector remains problematic, the optimal quotient will be ~1 theory per core*)
 
-LaunchKernels[];
-
+try=LaunchKernels[];
+Print@try;
 If[!OptionValue@"Import",
 Jobs=ParallelSubmit@DefTheoryParallel[#2,"Export"->#1]&@@@InputBatch;
 HiGGSPrint[Jobs];
