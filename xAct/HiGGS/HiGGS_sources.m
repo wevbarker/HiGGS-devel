d0cf0a3cf2fcc969ad74129364cd81b26a799cb0
William Barker <wb263@mrao.cam.ac.uk>
2022-05-02 18:46:37 +0100

    watch theorynames

--- xAct/HiGGS/HiGGS_sources.m
+++ xAct/HiGGS/HiGGS_sources.m
@@ -5355,7 +5355,7 @@ $TryKernels=True;
 While[$TryKernels,
 HiGGSPrint[" ** StudyTheory: Attempting to launch kernels"];
 CloseKernels[];
-TimeConstrained[LaunchKernels[32];
+TimeConstrained[Check@LaunchKernels[32];
 $TryKernels=False;,
 10,
 CloseKernels[];
