f02171764123710effb4d641719cdb09e8770f2e
William Barker <wb263@mrao.cam.ac.uk>
2022-05-07 15:36:09 +0100

    tinker

--- peta4.job.m
+++ peta4.job.m
@@ -20,15 +20,15 @@
 
 
 (* ::Input::Initialization:: *)
-HiGGS`$Timing=True;
+xAct`HiGGS`$Timing=True;
 
 
 (* ::Input::Initialization:: *)
 If[IntegerQ@ToExpression@$CommandLine[[-1]],
-HiGGS`$Node=$CommandLine[[-1]];
-ToExpression@("<<"<>FileNameJoin@{Directory[],"bin","node-"<>HiGGS`$Node,"JobsBatch.mx"}<>";");
+xAct`HiGGS`$Node=$CommandLine[[-1]];
+ToExpression@("<<"<>FileNameJoin@{Directory[],"bin","node-"<>xAct`HiGGS`$Node,"JobsBatch.mx"}<>";");
 Print@JobsBatch;,
-HiGGS`$Node="";
+xAct`HiGGS`$Node="";
 JobsBatch={{"spin_0p",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp5==0,2Bet1+Bet2==0,Bet1+2Bet3==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},
 {"spin_0m",{Alp1==0,Alp2==0,Alp4==0,Alp5==0,Alp6==0,2Bet1+Bet2==0,Bet1+2Bet3==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},
 {"simple_spin_1p",{Alp1==0,Alp2==0,Alp3==0,Alp4==0,Alp6==0,Bet1==0,Bet2==0,cAlp1==0,cAlp2==0,cAlp3==0,cAlp4==0,cAlp5==0,cAlp6==0,cBet1==0,cBet2==0,cBet3==0}},
