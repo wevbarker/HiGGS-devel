59c625483d29ab42986e2c9928d655ed5c0a9f56
William Barker <wb263@mrao.cam.ac.uk>
2022-05-08 18:45:25 +0100

    attempt to scrub old times

--- peta4.job.sh
+++ peta4.job.sh
@@ -6,18 +6,19 @@ echo "welcome to peta4.job.sh"
 echo "moving to HiGGS_development"
 cd /home/wb263/HiGGS_development
 
-#	flush the stats directory
-echo "flushing stats directory and build time"
-rm -rf ./svy/node-$1/chr
-mkdir ./svy/node-$1/chr
-rm ./svy/node-$1/peta4.chr.mx
-rm ./svy/node-$1/peta4.nom.mx
-
 echo "commencing run loop"
 
 #	run the job	
 while [ $(ls ./svy/node-$1/chr | wc -l) -le 32 ]; do
   echo "fewer than 32 kernel files were found in the stats directory, running script peta4.job.m"
+
+  #	flush the stats directory
+  echo "flushing stats directory and build time"
+  rm -rf ./svy/node-$1/chr
+  mkdir ./svy/node-$1/chr
+  rm ./svy/node-$1/peta4.chr.mx
+  rm ./svy/node-$1/peta4.nom.mx
+
   math -run < peta4.job.m $1
   echo "script peta4.job.m has exited and returned to peta4.job.sh"
 done
