da9f5cea1085a21c5385215500bff0706e000847
William Barker <wb263@mrao.cam.ac.uk>
2022-05-08 17:23:14 +0100

    hopefully now running

--- appcg.plt.sh
+++ appcg.plt.sh
@@ -5,7 +5,7 @@ module load anaconda/python3
 #	this file prepares a kernel report and sends it to the tower by ssh 
 while true; do
   echo "			(...replotting...)"
-  ./appcs.plt.py > /dev/null 2>&1
-  scp ./appcs.plt.png tower:~/Documents/physics/projects/HiGGS_development/ 
+  ./appcg.plt.py > /dev/null 2>&1
+  scp ./appcg.plt.png tower:~/Documents/physics/projects/HiGGS_development/ 
   #./peta4_jobs_plot.py > /dev/null 2>&1 	#	 for quiet plotting
 done
