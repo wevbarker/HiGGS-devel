9246397328ed5a3cbb271557e69e3b6668a01e39
William Barker <wb263@cam.ac.uk>
2022-11-06 14:02:38 +0000

    improved error handling during the build process, should allow users to rebuild context binaries automatically for fresh installs

--- xAct/HiGGS/Sources/Package/BuildHiGGS.m
+++ xAct/HiGGS/Sources/Package/BuildHiGGS.m
@@ -30,27 +30,41 @@ LoadHiGGS[]:=Catch@Module[{PrintVariable,InitialMemory,Progress},
 	NotebookDelete@Progress;
 ];
 
-$HiGGSBuilt=False;
-BuildHiGGS::built="The HiGGS environment has already been built.";
 Options@BuildHiGGS={Recompile->False};
-BuildHiGGS[OptionsPattern[]]:=Module[{PriorMemory,UsedMemory},
+BuildHiGGS[OptionsPattern[]]:=Module[{PrintVariable},
 
 	If[$HiGGSBuilt,Throw@Message[BuildHiGGS::built]];
 
 		If[OptionValue@Recompile,
 			
-			PriorMemory=MemoryInUse[];
-			Print[" ** BuildHiGGS: RAM used by kernel ",$KernelID," is ",Dynamic[Refresh[MemoryInUse[],UpdateInterval->1]]," bytes."];
-
+			Print@" ** BuildHiGGS: A rebuild of the context binaries was requested by an edit to HiGGS.m...";
 			BuildRebuild["BuildHiGGS.m"];
+			SaveBinaryContexts[];
+			Print@" ** BuildHiGGS: The context binaries have been rebuilt, the kernel will
+			now quit. Please reload HiGGS.";
+			Quit[];,
 
-			SaveBinaryContexts[],
+			Quiet@Check[
 
-			LoadHiGGS[];
+				PrintVariable=PrintTemporary@" ** BuildHiGGS: Attempting to load HiGGS as usual
+				from context binaries...";
+				LoadHiGGS[];,
+
+				Print@" ** BuildHiGGS: No suitable context binaries found, rebuilding them from
+				scratch. Don't worry: this should happen if it is your first time loading the
+				package from a fresh install! It is *very important* that HiGGS is installed to
+				a place where Mathematica has permissions to write files. For example on linux
+				this might be ~/.Mathematica/Applications/xAct/HiGGS/.";
+				BuildRebuild["BuildHiGGS.m"];
+				SaveBinaryContexts[];
+				Print@" ** BuildHiGGS: The context binaries have been rebuilt, the kernel will
+				now quit. Please reload HiGGS.";
+				Quit[];
+			];
+
+			NotebookDelete@PrintVariable;	
 			Begin["xAct`HiGGS`Private`"];
 				BuildHiGGSPackage[];
 			End[];
 		];
-
-	$HiGGSBuilt=True;
 ];
