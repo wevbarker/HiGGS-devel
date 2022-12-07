3ed0d0122d6036770c6dac3ea3ab7e86f50a8ee9
William Barker <wb263@cam.ac.uk>
2022-12-07 19:13:28 +0000

    now remembering that we need to actually import the TEGR theory!

--- ManuelBugReport.m
+++ ManuelBugReport.m
@@ -6,6 +6,10 @@ Print@"First load the latest version of the HiGGS package:";
 
 <<xAct`HiGGS`;
 
+Print@"We are working in the context of the TEGR, so we need to load it into memory. I didn't document this, but we can pass some options to hide basically all the details:"
+
+ViewTheory["TEGR", Literature->False, Brackets->False, Velocities->False];
+
 Print@"Now we want to construct a specific combination of constraints:";
 
 Combination = (PhiB1p[-i,-j]-PhiB1p[-j,-i]+V[-i]*PhiB1m[-j]-V[-j]*PhiB1m[-i]);
