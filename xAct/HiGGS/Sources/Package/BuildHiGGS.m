8b7906f75e87f5f51c38647d1fc8efd6a3a1d8fd
William Barker <wb263@cam.ac.uk>
2023-02-22 18:26:20 +0000

    Tidied directory structure.

--- xAct/HiGGS/Sources/Package/BuildHiGGS.m
+++ xAct/HiGGS/Sources/Package/BuildHiGGS.m
@@ -25,8 +25,10 @@ BuildHiGGS[OptionsPattern[]]:=Module[{PrintVariable},
 
 				PrintVariable=PrintTemporary@" ** BuildHiGGS: Attempting to load HiGGS as usual
 				from context binaries...";
-				LoadHiGGS[];,
-(*
+				Off[General::shdw];
+				LoadHiGGS[];
+				On[General::shdw];,
+(**)
 				Print@" ** BuildHiGGS: No suitable context binaries found, rebuilding them from
 				scratch. Don't worry: this should happen if it is your first time loading the
 				package from a fresh install! It is *very important* that HiGGS is installed to
@@ -37,7 +39,7 @@ BuildHiGGS[OptionsPattern[]]:=Module[{PrintVariable},
 				Print@" ** BuildHiGGS: The context binaries have been rebuilt, the kernel will
 				now quit. Please reload HiGGS.";
 				Quit[];
-*)
+(**)
 			];
 
 			NotebookDelete@PrintVariable;	
